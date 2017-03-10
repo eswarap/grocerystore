db8b7c87ae5482690b55796ac5f8443a832e2e43
eswara <eswaraprasadh@yahoo.com>
2017-03-10 10:01:11 +0000

    (*) Order,OrderItem

--- src/main/java/com/woven/grocerystore/jpa/Order.java
+++ src/main/java/com/woven/grocerystore/jpa/Order.java
@@ -12,6 +12,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -37,7 +39,12 @@ public class Order implements Serializable {
     private Long orderId;
     
     @OneToOne(fetch = FetchType.LAZY,cascade=CascadeType.PERSIST)
+    @JoinColumn(name="CUSTOMER_ID")
     private Customer customer;
+    
+    @OneToMany(fetch = FetchType.LAZY,cascade=CascadeType.PERSIST)
+    @JoinColumn(name="ORDERITEM_ID")
+    private OrderItem orderItem;
     
     @Column(name="ORDER_STATUS")
     private String orderStatus;
@@ -46,7 +53,16 @@ public class Order implements Serializable {
     private Date orderDate;
     
     @Column(name="TOTAL")
-    private int total;
+    private Integer total;
     
-    
+    @Override
+    public String toString() {
+        return new StringBuilder().append("ORDER_ID:").append(orderId).
+                append(" customer:").append(customer==null?"null":customer.toString()).
+                append(" orderItem:").append(orderItem==null?"null":orderItem.toString()).
+                append(" orderStatus:").append(orderStatus).
+                append(" orderDate:").append(orderDate).
+                append(" TOTAL:").append(total.toString()).
+                toString();
+    }
 }
--- src/main/java/com/woven/grocerystore/jpa/OrderItem.java
+++ src/main/java/com/woven/grocerystore/jpa/OrderItem.java
@@ -1,10 +1,19 @@
 package com.woven.grocerystore.jpa;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType; 
+import javax.persistence.JoinColumn;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 @Access(AccessType.FIELD)
@@ -12,4 +21,43 @@ public class OrderItem implements Serializable {
     
      private static final long serialVersionUID = 1l;
      
+     @Id
+     @Column(name="ORDERITEM_ID")
+     @GeneratedValue(strategy=GenerationType.AUTO) 
+     private Long orderItemId;
+     
+     @OneToOne(fetch = FetchType.LAZY,cascade=CascadeType.PERSIST)
+     @JoinColumn(name="ORDER_ID")
+     private Order order;
+
+     @OneToOne(fetch = FetchType.LAZY,cascade=CascadeType.PERSIST)
+     @JoinColumn(name="PRODUCT_ID")
+     private Product product;
+    
+     @Column(name="UNIT_PRICE")
+     private BigDecimal unitPrice;
+     
+     @Column(name="QUANTITY")
+     private Integer quantity;
+     
+     public OrderItem() {
+      
+     }
+     
+     public OrderItem(Order order,Product product,BigDecimal unitPrice,int quantity) {
+         this.order = order;
+         this.product = product;
+         this.unitPrice = unitPrice;
+         this.quantity = quantity;
+     }
+     
+     @Override
+    public String toString() {
+        return new StringBuilder().append("ORDERITEM_ID:").append(orderItemId).
+                append(" order:").append(order==null?"null":order.toString()).
+                append(" product:").append(product==null?"null":product.toString()).
+                append(" unitPrice:").append(unitPrice.toString()).
+                append(" quantity:").append(quantity.toString()).
+                toString();
+    }
 }
