ffbef677aac7e543f1018703a7edca7ae36b8799
Eswara Prasadh <eswaraprasadh@yahoo.com>
2017-03-13 06:41:36 +0000

    jndi test cas

---
+++ src/test/java/com/woven/grocerystore/database/JndiDBConnectionTest.java
@@ -0,0 +1,31 @@
+package com.woven.grocerystore.database;
+
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:config/spring/prod-context.xml"})
+@ActiveProfiles("jpa-pgsql")
+public class JndiDBConnectionTest {
+   
+  @Autowired(required=true)
+  private DataSource dataSource;
+   
+   @Test
+   public void testDbConnection() throws Exception {
+        Connection conn = dataSource.getConnection();   
+        Assert.assertNotNull(conn);
+        
+    }
+   
+}
