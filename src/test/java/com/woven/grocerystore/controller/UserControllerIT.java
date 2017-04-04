185556a1a7dfdb56203f7d521a1b8dc5889d9b22
Eswara <eswaraprasadh@yahoo.com>
2017-04-04 06:42:55 +0530

    spring secruity saga continues

--- src/test/java/com/woven/grocerystore/controller/UserControllerIT.java
+++ src/test/java/com/woven/grocerystore/controller/UserControllerIT.java
@@ -22,7 +22,7 @@ public class UserControllerIT extends BaseControllerIT {
                 .param("userName", "admin")
                 .param("password", "password"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("redirect:/hello"))
+                .andExpect(view().name("user/login"))
                 .andExpect(model().hasNoErrors())
                 .andDo(MockMvcResultHandlers.log());
     }
